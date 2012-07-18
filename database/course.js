c18e26f4479cbb780f91a18a2da29c27dc393c14
Justin Ma <hustcer@gmail.com>
2012-07-18 16:23:30 +0800

    fix course and style

--- database/course.js
+++ database/course.js
@@ -15,24 +15,24 @@ var cCourseLatin = {
 	// **************	爵士数据存储在dance数据库中的	jazz   collecion中 		*************
 	// **************	街舞数据存储在dance数据库中的	hiphop collecion中 		*************
 	courseType : 'latin',
-	// 每个课程可以有：cCapacity、locked、autoApprove、manFirst、autoLimit等局部配置，可以覆盖相应的全局配置
-	courses    :[{cValue: "13SE", cName: "第13期桑巴基础班", ps:"上课时间：每周四晚6:30~7:30",locked: false},
-	 			    {cValue: "13CI", cName: "第13期恰恰中级班", ps:"上课时间：每周四晚7:40~8:40",locked: false}],
-	// 课程额定容量，此为全局配置可以被各课程的对应配置所覆盖
-	cCapacity  : 21, 
-	// 锁定课程状态，禁止报名、退课等操作
-	locked     : false,
-	// 是否开启报名自动审核
-	autoApprove: true,
-	// 是否男士报名优先，对于拉丁课男士优先则自动审核开启时男士报名自动审核通过
-	manFirst   : true,
-	// 自动审核报名成功的人数限制
-	autoLimit  : 18,
-   notice     : '上课地点:公司舞房.每个培训舞种共12次课,累计费用120.00元,开课时间05.17,具体情况等通知.报名成功的同学请付款到本人支付宝: http://me.alipay.com/hustcer,逾期未付课程将被取消.',
-   successMsg : '您好，您的拉丁培训报名申请已经审核通过，烦请迅速缴费，逾期未付则课程将被取消。敬请谅解!',
+   // 每个课程可以有：cCapacity、locked、autoApprove、manFirst、autoLimit等局部配置，可以覆盖相应的全局配置
+   courses    :[{cValue: "14RE", cName: "伦巴基础班", ps:"上课时间：每周四晚6:30~7:30",locked: false},
+                {cValue: "14CE", cName: "恰恰基础班", ps:"上课时间：每周四晚7:40~8:40",locked: false}],
+   // 课程额定容量，此为全局配置可以被各课程的对应配置所覆盖
+   cCapacity  : 15,
+   // 锁定课程状态，禁止报名、退课等操作
+   locked     : true,
+   // 是否开启报名自动审核
+   autoApprove: true,
+   // 是否男士报名优先，对于拉丁课男士优先则自动审核开启时男士报名自动审核通过
+   manFirst   : true,
+   // 自动审核报名成功的人数限制
+   autoLimit  : 15,
+   notice     : '上课地点:公司舞房.每个培训舞种共6次课,本期培训免费,开课时间07.05.如果您13期课程已经报名过不用重复报名,可以直接去上课,之前未报名的需要在此登记.谢谢合作.',
+   successMsg : '您好，您的拉丁培训报名申请已经审核通过，请按时上课，若因故不能上课可以取消或者申请退课!',
    quitMsg    : '您好，您的拉丁退课申请已经审核通过，欢迎下次报名!',
-	beginDate  : '',
-	endingDate : ''
+   beginDate  : '',
+   endingDate : ''
 };
 
 /**
@@ -40,15 +40,14 @@ var cCourseLatin = {
  */
 var cCourseJazz = {
    courseType  : 'jazz', 
-   courses     :[{cValue: "7JZE1", cName: "第7期JAZZ基础1班", ps:"上课时间：每周一晚7:40~8:30", cCapacity: 20, autoLimit: 20},
-	 			     {cValue: "7JZE2", cName: "第7期JAZZ基础2班", ps:"上课时间：每周二晚7:40~8:30", cCapacity: 20, autoLimit: 20},
-	 			     {cValue: "7JZA1",  cName: "第7期JAZZ提高1班",  ps:"上课时间：每周一晚6:40~7:30", cCapacity: 20, autoLimit: 20},
-	 			     {cValue: "7JZA2",  cName: "第7期JAZZ提高2班",  ps:"上课时间：每周二晚6:40~7:30", cCapacity: 20, autoLimit: 20}],
+   courses     :[{cValue: "7JZE1", cName: "第7期JAZZ基础1班", ps:"上课时间：每周一晚7:40~8:30"},
+	 			     {cValue: "7JZE2", cName: "第7期JAZZ基础2班", ps:"上课时间：每周二晚7:40~8:30"},
+	 			     {cValue: "7JZA1", cName: "第7期JAZZ提高1班", ps:"上课时间：每周一晚6:40~7:30"},
+	 			     {cValue: "7JZA2", cName: "第7期JAZZ提高2班", ps:"上课时间：每周二晚6:40~7:30", cCapacity: 20, autoLimit: 20}],
 
- 
    cCapacity   : 20, 
    // 锁定课程状态，禁止报名、退课等操作
-   locked      : true,
+   locked      : false,
    // 是否开启报名自动审核
    autoApprove : true,
    // 是否男士报名优先，对于拉丁课男士优先则自动审核开启时男士报名自动审核通过
@@ -91,6 +90,8 @@ var cCourseHiphop = {
  * 最后一个大写字母表示课程级别：E: Elementary 基础班;I: Intermediate 中级班; A: Advanced 高级班
  */
 var courseLatinList = [
+      {courseVal:'14RE', courseName:'伦巴基础班(临时)'},
+      {courseVal:'14CE', courseName:'恰恰基础班(临时)'},
 		{courseVal:'13SE', courseName:'第13期桑巴基础班'},
 		{courseVal:'13CI', courseName:'第13期恰恰中级班'}
 	];
@@ -101,8 +102,8 @@ var courseLatinList = [
  * 最后一个大写字母表示课程级别：E: Elementary 基础班;I: Intermediate 中级班; A: Advanced 高级班
  */
 var courseJazzList = [
-		{courseVal:'7JZA1',  courseName:'第7期Jazz提高1班'},
-		{courseVal:'7JZA2',  courseName:'第7期Jazz提高2班'},
+		{courseVal:'7JZA1', courseName:'第7期Jazz提高1班'},
+		{courseVal:'7JZA2', courseName:'第7期Jazz提高2班'},
 		{courseVal:'7JZE1', courseName:'第7期Jazz基础1班'},
 		{courseVal:'7JZE2', courseName:'第7期Jazz基础2班'},
 		{courseVal:'6JZI',  courseName:'第6期Jazz中级班'},
