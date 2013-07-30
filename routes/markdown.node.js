e84b19ead76ed307724aa10fc5b834746081d752
majun <majun@shterm.com>
2013-07-30 16:31:30 +0800

    fix app errors, update npm

--- routes/markdown.node.js
+++ routes/markdown.node.js
@@ -3,15 +3,15 @@
  * GET and render markdown pages.
  *
  * Author: 	hustcer
- * Date: 	2012-05-22  
+ * Date: 	2012-05-22
  */
 
 var fs          = require('fs'),
-    md          = require('discount'),
+    md          = require('markdown').markdown,
     path        = require('path');
 
 exports.mdrender= function(req, res){
-	
+
     var markdownFile    = path.normalize(__dirname + '/../doc/' + req.params.md + '.md');
 
     // 对应的markdown文件不存在则重定向到404
@@ -27,11 +27,11 @@ exports.mdrender= function(req, res){
             res.redirect('/err404');
         }
 
-        var head = '<head><title>Alibaba舞蹈培训 - ' + req.params.md + '</title>' + 
+        var head = '<head><title>Alibaba舞蹈培训 - ' + req.params.md + '</title>' +
             '<link rel="stylesheet" href="/stylesheets/style.css"><link rel="stylesheet" href="/stylesheets/fdev-float.css"></head>';
-        var html = head + '<body class="content markdown">' + md.parse(data) + '</body>';
+        var html = head + '<body class="content markdown">' + md.toHTML(data) + '</body>';
 
         res.send(html);
     });
-    
+
 };
